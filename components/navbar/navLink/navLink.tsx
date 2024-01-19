7cdbf78555279e1e06412d01145a06ae48adda74
ruzive <ruzive@gmail.com>
2024-01-19 14:52:54 +0200

    added navbar with feature to show active page

--- components/navbar/navLink/navLink.tsx
+++ components/navbar/navLink/navLink.tsx
@@ -1,15 +1,21 @@
+'use client'
+
 import Link from "next/link"
+import styles from './navLink.module.css'
 import { usePathname } from "next/navigation"
 
-export const NavLink = ({item}) => {
-
+const NavLink = ({ item }) => {
     const pathName = usePathname()
-
   return (
-    <div>
-        <Link href={item.path} className={'container' ${pathName === item.title &&}}>
+        <Link 
+            href={item.path} 
+            className={`${styles.container} ${
+                pathName === item.path && styles.active
+                }`}
+                >
             {item.title}
         </Link>
-    </div>
   )
 }
+
+export default NavLink
