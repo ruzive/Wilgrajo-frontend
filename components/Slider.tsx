581c7ea7b90dd2ba0401c00b7dc21a80ad5cbc7b
ruzive <ruzive@gmail.com>
2024-02-12 10:26:53 +0200

    details page

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,39 +1,83 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import Image from 'next/image'
-import City from "../public/assets/city.jpg"
-// import { PropertyType } from '../typings'
-// interface Props {
-//     property: PropertyType,
+// import { Swiper, SwiperSlide } from "swiper/react";
+ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+// import Image from 'next/image'
+//  import {DataAttributes} from '@app/utils/utils'
+
+// const Slider = ({ photos }: DataAttributes) => {
+//   return (
+//     <Swiper
+//         slidesPerView={1}
+//         spaceBetween={30}
+//         slidesPerGroup={1}
+//         loop={true}
+//         pagination={{
+//           clickable: true,
+//         }}
+//         navigation={true}
+//         modules={[Pagination, Navigation]}
+//         className="mySwiper h-full w-full rounded-xl"
+//       >
+//         {
+//           photos.map((pic) => (
+//             <SwiperSlide key={pic.id} className='flex justify-center items-center min-[1200px]:px-[1px] px-[1px]'>
+//               <img 
+//                 className="rounded-xl" 
+//                 src={pic.image} 
+//                 alt="property"/>
+//             </SwiperSlide>
+//           ))
+//         }
+//       </Swiper>
+//   )
 // }
 
-const Slider = ({ property }: Props) => {
-  return (
-    <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        slidesPerGroup={1}
-        loop={true}
-        loopFillGroupWithBlank={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper h-full w-full rounded-xl"
-      >
-        {
-          property.images.map((image: string) => (
-            <SwiperSlide key={property._id} className='flex justify-center items-center min-[1200px]:px-[1px] px-[1px]'>
-              <img className="rounded-xl" src={image} alt=""/>
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
-  )
+// export default Slider
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { DataAttributes, Photo } from '@app/utils/utils';
+import Image from 'next/image';
+
+interface SliderProps {
+  photos: Photo[];
 }
 
-export default Slider
+const Slider = ({ photos }: SliderProps) => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      slidesPerGroup={1}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }}
+      className="mySwiper h-full w-full rounded-xl"
+    >
+      {photos.map((pic) => (
+        <SwiperSlide key={pic.id} className='flex justify-center items-center min-[1200px]:px-[1px] px-[1px]'>
+          <Image
+            src={pic.image[0]} // Assuming the image URL is stored at index 0
+            alt="property"
+            width={500} // Adjust width and height as per your design
+            height={300}
+            className="rounded-xl"
+          />
+        </SwiperSlide>
+      ))}
+      <div className="swiper-button-next"></div>
+      <div className="swiper-button-prev"></div>
+    </Swiper>
+  );
+};
+
+export default Slider;
