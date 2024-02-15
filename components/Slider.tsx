1325263ab1d718ca0744f62c1594ea00103e9c98
ruzive <ruzive@gmail.com>
2024-02-15 12:14:43 +0200

    details

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,41 +1,6 @@
-// import { Swiper, SwiperSlide } from "swiper/react";
- import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
- import Image from 'next/image'
-//  import {DataAttributes} from '@app/utils/utils'
 
-// const Slider = ({ photos }: DataAttributes) => {
-//   return (
-//     <Swiper
-//         slidesPerView={1}
-//         spaceBetween={30}
-//         slidesPerGroup={1}
-//         loop={true}
-//         pagination={{
-//           clickable: true,
-//         }}
-//         navigation={true}
-//         modules={[Pagination, Navigation]}
-//         className="mySwiper h-full w-full rounded-xl"
-//       >
-//         {
-//           photos.map((pic) => (
-//             <SwiperSlide key={pic.id} className='flex justify-center items-center min-[1200px]:px-[1px] px-[1px]'>
-//               <img 
-//                 className="rounded-xl" 
-//                 src={pic.image} 
-//                 alt="property"/>
-//             </SwiperSlide>
-//           ))
-//         }
-//       </Swiper>
-//   )
-// }
-
-// export default Slider
-
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import Image from 'next/image'
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
