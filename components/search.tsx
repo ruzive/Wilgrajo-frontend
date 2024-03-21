94646f2679c7f66554f54ad5bcceec1a9f470936
ruzive <ruzive@gmail.com>
2024-03-21 14:31:26 +0200

    fixed build issues

--- components/search.tsx
+++ components/search.tsx
@@ -4,6 +4,7 @@ import { HiMagnifyingGlass } from "react-icons/hi2";
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import ErrorBoundary from "@app/utils/error";
+import { Suspense } from "react";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -26,25 +27,27 @@ export default function Search({ placeholder }: { placeholder: string }) {
   ,300);
   return (
     <ErrorBoundary>
-    <div className="relative flex flex-1 flex-shrink-0 w-[100%]">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-      className=" z-10 flex justify-between 
-      items-center text-neutral-600 
-      placeholder-neutral-600 bg-neutral-500/40 
-      rounded-lg shadow-2xl w-full py-[9px] 
-      pl-10 min-[1200px]:my-[50px] my-[25px] 
-      font-bold focus:outline focus:ring-neutral-400"
-        placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('query')?.toString()}
-      />
-      <HiMagnifyingGlass className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
+        <Suspense>
+                <div className="relative flex flex-1 flex-shrink-0 w-[100%]">
+                  <label htmlFor="search" className="sr-only">
+                    Search
+                  </label>
+                  <input
+                  className=" z-10 flex justify-between 
+                  items-center text-neutral-600 
+                  placeholder-neutral-600 bg-neutral-500/40 
+                  rounded-lg shadow-2xl w-full py-[9px] 
+                  pl-10 min-[1200px]:my-[50px] my-[25px] 
+                  font-bold focus:outline focus:ring-neutral-400"
+                    placeholder={placeholder}
+                    onChange={(e) => {
+                      handleSearch(e.target.value);
+                    }}
+                    defaultValue={searchParams.get('query')?.toString()}
+                  />
+                  <HiMagnifyingGlass className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
+    </Suspense>
      </ErrorBoundary>
   );
 }
