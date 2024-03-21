9c822893b65d7634e5e20f80853375e77ee208a6
ruzive <ruzive@gmail.com>
2024-03-21 17:18:36 +0200

    contact page error on image

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -77,13 +77,13 @@ const ContactForm: React.FC = () => {
         <div className="p-6">
           <section className="mb-4">
             <div className="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat ">
-              <Image 
+              {/* <Image 
                 alt='background-contact' 
                 src='/public/assets/images/zimindo_plaza.jpeg' 
                 fill
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 priority 
-              />
+              /> */}
             </div>
             <div className="px-6 md:px-12">
               <div className="block rounded-lg bg-[hsla(0,0%,100%,0.7)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
