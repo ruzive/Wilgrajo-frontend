0e5892184dbf982efedb4a1183b3da8faaecc0b7
ruzive <ruzive@gmail.com>
2024-03-22 15:57:08 +0200

    removed banner on layout for testing

--- app/(overview)/layout.tsx
+++ app/(overview)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@styles/globals.css'
 import Footer from '@components/Footer/footer'
-import Banner from '@components/Banner' 
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +21,7 @@ export default function HomeLayout({
     <main className='flex flex-col lg:flex-row justify-end min-h-screen'> 
         <section 
             className='lg:fixed lg:left-0 rounded-lg flex flex-col justify-center lg:w-[31%] bg-gray-50 lg:h-screen mb-[20px] lg:mb-[0px]  mx-auto'> 
-            <Banner placeholder='Search by intent, location and condition.'/>
+            {/* <Banner placeholder='Search by intent, location and condition.'/> */}
             <Footer/>
         </section>
          <section className='relative transparent lg:w-[69%]'>
