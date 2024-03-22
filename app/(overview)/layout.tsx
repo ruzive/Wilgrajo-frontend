ce3624e95c64dcbc61d57557e2ea6f6ba0f70d87
ruzive <ruzive@gmail.com>
2024-03-22 10:38:41 +0200

    removed import banner from root layout

--- app/(overview)/layout.tsx
+++ app/(overview)/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Banner from '@components/Banner'
 import '@styles/globals.css'
-import Nav from '@components/navbar/Nav'
 import Footer from '@components/Footer/footer'
-import Cards from '@components/Cards'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,34 +18,6 @@ export default function HomeLayout({
 }) 
 {
   return (
-    // <html lang="en">
-    //   <body className={inter.className}>
-    //     
-    
-    //       <section 
-    //         className='lg:fixed lg:left-0 flex flex-col justify-center lg:w-[31%] lg:h-screen mt-[50px] lg:mt-[0px] mb-[20px] lg:mb-[0px]'> 
-    //         <Banner placeholder='Search by intent, location and condition.'/>
-    //         <Footer/>
-    //       </section>
-    //       <section className='relative bg-gradient-to-tr from-[#558b00]/60 to-[#fe3e0a]/60 lg:w-[69%]'>
-    //         <Nav/>
-    //         {children}
-    //       </section>
-    //     </main>
-    //   </body>
-    // </html>
-
-    // <main className='flex flex-col lg:flex-row justify-end min-h-screen'> 
-    //   {/* <section className='lg:fixed lg:left-0 flex flex-col justify-center lg:w-[31%] lg:h-screen mt-[50px] lg:mt-[0px] mb-[20px] lg:mb-[0px]'> 
-    //     <Banner placeholder='Search by intent, location and condition.'/>
-    //     <Footer/>
-    //   </section> */}
-    //   <section className='relative bg-gradient-to-tr from-[#558b00]/60 to-[#fe3e0a]/60 lg:w-[69%]'>
-    //     {children}
-    //   </section>
-    // </main>
-
-    // <div className='flex flex-col lg:flex-row justify-end min-h-screen'>
     <main className='flex flex-col lg:flex-row justify-end min-h-screen'> 
         <section 
             className='lg:fixed lg:left-0 rounded-lg flex flex-col justify-center lg:w-[31%] bg-gray-50 lg:h-screen mb-[20px] lg:mb-[0px]  mx-auto'> 
@@ -58,8 +28,5 @@ export default function HomeLayout({
             {children}
         </section>
     </main>
-    // </div>
-
-
   )
 };
